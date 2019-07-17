592083600c464323d63bba683a9d6c8f3c4b6cbb
Yerlan Yeszhanov <yerlanyeszhanov@iMac.local>
2019-07-17 16:45:39 +0600

    fix:table

--- resources/js/components/ui/Table.jsx
+++ resources/js/components/ui/Table.jsx
@@ -1,105 +1,43 @@
-// import React, { Fragment, useState, useEffect } from "react";
-// import styled, { css } from "styled-components";
-
-// const TableWrapperUI = styled.div`
-//   display: grid;
-//   box-sizing: border-box;
-//   width: 100%;
-//   overflow:hidden;
-//   border: 1px solid #dbeaf4;
-//   grid-template-columns: repeat(
-//     ${props => props.columns && props.columns},
-//     fit-content(400px)
-//   );
-//   justify-items: center;
-//   padding: 5px 0;
-//   justify-content: space-between;
-//   > span {
-//     padding: 5px;
-//     justify-self: left;
-//     position: relative;  
-//     :hover {
-//       background: #dbeaf4;
-//     }
-//     :hover::before, ::before {
-//       cursor:pointer;
-//       content: "";
-//       position: absolute;
-//       background: inherit;
-//       top: 0;
-//       bottom: 0;
-//       left: -100vw;
-//       right: -100vw;
-//     }
-//     :hover::before {
-//       z-index:-1
-//     }
-//   }
-// `;
-
-// const LineUI = styled.div`
-//   border-bottom: 1px solid #dbeaf4;
-//   width: 100%;
-//   grid-column: 1 / -1;
-// `;
-
-// const Table = ({ children, titles, data }) => {
-//   const [amountColumns, setAmountColumns] = useState(0);
-//   useEffect(() => {
-//     setAmountColumns(titles.length);
-//   }, []);
-
-//   const displayData = data => {
-//     return data.map((x, idx) => {
-//       return (
-//         <Fragment key={idx}>
-//           {Object.keys(x).map((value, ids) => (
-//             <span key={ids}>{x[value]}</span>
-//           ))}
-//           <LineUI />
-//         </Fragment>
-//       );
-//     });
-//   };
-
-//   const displayTitles = titles => {
-//     return titles.map((title, idx) => {
-//       return <span key={idx}>{title}</span>;
-//     });
-//   };
-
-//   return (
-//     amountColumns > 0 && (
-//       <TableWrapperUI columns={amountColumns}>
-//         {displayTitles(titles)}
-//         <LineUI /> {displayData(data)}
-//       </TableWrapperUI>
-//     )
-//   );
-// };
-// export default Table;
-
 
 
 import React, { Fragment, useState, useEffect } from "react";
 import styled, { css } from "styled-components";
 
 const TableWrapperUI = styled.table`
-width:100%;
+  width: 100%;
+  border-spacing: 0;
 `;
 const TableTdUI = styled.td`
-  padding:0;
+  padding: 10px 3px;
 `;
 const TableThUI = styled.th`
-text-align: left;
+  text-align: left;
+  padding: 10px 3px;
+  font: 600 12px/15px "Source Sans Pro";
+  color: #737a9b;
 `;
+
+
 const TableTrUI = styled.tr`
   cursor: pointer;
 
   :hover {
-    background: lightblue;
+    background: #edf4f9;
   }
 `;
+const TableTHeadUI = styled.thead`
+  padding: 0 10px;
+  ${TableTrUI} {
+    :hover {
+      background: none;
+      cursor: auto;
+ 
+  }
+`;
+
+const TableTBodyUI = styled.tbody`
+
+`
 
 const Table = ({ children, titles, data }) => {
   const [amountColumns, setAmountColumns] = useState(0);
@@ -111,11 +49,9 @@ const Table = ({ children, titles, data }) => {
     return data.map((x, idx) => {
       return (
         <TableTrUI key={idx}>
-         
-            {Object.keys(x).map((value, ids) => (
-              <TableTdUI key={ids}>{x[value]}</TableTdUI>
-            ))}
-          
+          {Object.keys(x).map((value, ids) => (
+            <TableTdUI key={ids}>{x[value]}</TableTdUI>
+          ))}
         </TableTrUI>
       );
     });
@@ -130,9 +66,10 @@ const Table = ({ children, titles, data }) => {
   return (
     amountColumns > 0 && (
       <TableWrapperUI columns={amountColumns}>
-
-<thead><TableTrUI>{displayTitles(titles)}</TableTrUI></thead>
-        <tbody>{displayData(data)}</tbody>
+        <TableTHeadUI>
+          <TableTrUI>{displayTitles(titles)}</TableTrUI>
+        </TableTHeadUI>
+        <TableTBodyUI>{displayData(data)}</TableTBodyUI>
       </TableWrapperUI>
     )
   );
